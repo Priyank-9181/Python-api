e88731f306a435bd63ef816b7a17f35caff699aa
Priyank-9181 <jaysukhshekhada11@gmail.com>
2025-03-16 14:40:34 +0530

    Some Change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 
+from ollama import chat
+from ollama import ChatResponse
+
 
 app = FastAPI()
 
@@ -7,3 +10,20 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+
+@app.post("/test")
+def test(prompt: str):
+    response: ChatResponse = chat(
+        model="llama3.2:1b",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+    )
+    print(response["message"]["content"])
+    # or access fields directly from the response object
+    print(response.message.content)
+    return {"message": prompt, "response": response.message.content}
